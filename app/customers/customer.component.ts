f0eb3db5615e11f64c2ee56dfb398cde7a7cd629
ubbn <ubbn@live.com>
2017-02-10 22:17:50 +0100

    Moved validations from view to component class

--- app/customers/customer.component.ts
+++ app/customers/customer.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, NgForm } from '@angular/forms';
+import { FormBuilder, FormGroup, NgForm, Validators } from '@angular/forms';
 
 import { Customer } from './customer';
 
@@ -16,11 +16,22 @@ export class CustomerComponent implements OnInit {
 
     ngOnInit(): void {
         this.customerForm = this.formBuilder.group({ 
-            firstName: '',
-            lastName: '',
-            email: '',
+            firstName: ['', [Validators.required, Validators.minLength(3)]],
+            lastName: ['', [Validators.required, Validators.minLength(50)]],
+            email: ['', [Validators.required, Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+")]],
+            phone: '',
+            notification: 'email',
             sendCatalog: true
         });
+    }
+
+    updateNotifiation(type: string): void {
+        const phoneControl = this.customerForm.get('phone');
+        if (type === 'text')
+            phoneControl.setValidators(Validators.required);
+        else
+            phoneControl.clearValidators();
+        phoneControl.updateValueAndValidity();
     }
 
     save() {
