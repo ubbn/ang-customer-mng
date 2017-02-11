67a3cb178a9aa7ab8d099f0dcdabbeed4f53a9a0
ubbn <ubbn@live.com>
2017-02-11 16:26:32 +0100

    Added custom validation functions on component class

--- app/customers/customer.component.ts
+++ app/customers/customer.component.ts
@@ -1,7 +1,29 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, NgForm, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormGroup, NgForm, ValidatorFn, Validators } from '@angular/forms';
 
 import { Customer } from './customer';
+
+// Customer validator function
+// It takes control object as a parameter and returns an object
+// if invalid, return name of control for error collection
+// If valid, returns null (meaning that there is no error)
+function validateRating(c: AbstractControl) : { [key: string] : boolean } | null {
+    if (c.value != undefined && (isNaN(c.value) || c.value < 1 || c.value >5))
+        return {'range': true};
+    return null;
+}
+
+// Customer validator function (with parameter)
+// It is factory function that returns validator function
+// In this way, custom validator function can use more parameters than default AbstractControl
+function validateRatingWithParam(minVal: number, maxVal: number) : ValidatorFn {
+    // wrapped anonymous function
+    return (c: AbstractControl) : { [key: string] : boolean } | null => {
+        if (c.value != undefined && (isNaN(c.value) || c.value < minVal || c.value > maxVal))
+            return {'range': true};
+        return null;
+    }
+}
 
 @Component({
     selector: 'my-signup',
@@ -21,6 +43,7 @@ export class CustomerComponent implements OnInit {
             email: ['', [Validators.required, Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+")]],
             phone: '',
             notification: 'email',
+            rating: ['', validateRatingWithParam(1, 5)], // ['', validateRating] <- without parameters
             sendCatalog: true
         });
     }
